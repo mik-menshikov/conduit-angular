e756a3b0811891208ccea28c12eab1a137ca3c81
Mikhail Menshchikov <mikhail.menshchikov@akvelon.com>
2021-12-03 14:50:34 +0300

    chore: cleanup

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToErrorMessagePipe } from './to-error-message.pipe';
 import { FollowButtonComponent } from './follow-button/follow-button.component';
-import { SampleComponent } from './sample/sample.component';
 
 @NgModule({
-  declarations: [ToErrorMessagePipe, FollowButtonComponent, SampleComponent],
+  declarations: [ToErrorMessagePipe, FollowButtonComponent],
   exports: [ToErrorMessagePipe, FollowButtonComponent],
   imports: [CommonModule],
 })
